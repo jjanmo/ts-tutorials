7a7ae3184681467a77d569273833d965c63242df
jjanmo <jjanmo07@hanmail.net>
2022-01-15 20:56:46 +0900

    feat: solve intro quiz

---
+++ project01/intro-quiz/app.js
@@ -0,0 +1,31 @@
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
+
+const $name = document.querySelector('#name');
+const $address = document.querySelector('#address');
+const $email = document.querySelector('#email');
+
+function fetchUser() {
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      const {
+        name,
+        address: { city, street, suite },
+        email,
+      } = json[0];
+
+      $name.textContent = name;
+      $email.textContent = email;
+      $address.textContent = `${city} ${street} ${suite}`;
+    })
+    .catch((e) => console.log('error :', e));
+}
+
+fetchUser();
+
+function init() {
+  fetchUser();
+}
+
+init();
