4572969b524d775f85b565818f69f55776948dde
jjanmo <jjanmo07@hanmail.net>
2022-02-27 21:15:40 +0900

    feat: add image frame whenever select

--- project01/generic-example/app.ts
+++ project01/generic-example/app.ts
@@ -1,62 +1,62 @@
 const animalOptions = [
   {
     text: 'cat',
-    url: './animals/cat.jpg',
+    url: './images/animals/cat.jpg',
   },
   {
     text: 'fox',
-    url: './animals/fox.jpg',
+    url: './images/animals/fox.jpg',
   },
   {
     text: 'hamster',
-    url: './animals/hamster.jpg',
+    url: './images/animals/hamster.jpg',
   },
   {
     text: 'lion',
-    url: './animals/lion.jpg',
+    url: './images/animals/lion.jpg',
   },
   {
     text: 'pig',
-    url: './animals/pig.jpg',
+    url: './images/animals/pig.jpg',
   },
   {
     text: 'rabbit',
-    url: './animals/rabbit.jpg',
+    url: './images/animals/rabbit.jpg',
   },
 ];
 
 const languageOptions = [
   {
     text: 'c#',
-    url: './languages/c#.png',
+    url: './images/languages/c-sharp.png',
   },
   {
     text: 'go',
-    url: './languages/go.png',
+    url: './images/languages/go.png',
   },
   {
     text: 'java',
-    url: './languages/java.png',
+    url: './images/languages/java.png',
   },
   {
     text: 'javascript',
-    url: './languages/javascript.png',
+    url: './images/languages/javascript.png',
   },
   {
     text: 'python',
-    url: './languages/python.png',
+    url: './images/languages/python.png',
   },
   {
     text: 'ruby',
-    url: './languages/ruby.png',
+    url: './images/languages/ruby.png',
   },
   {
     text: 'rust',
-    url: './languages/rust.png',
+    url: './images/languages/rust.png',
   },
   {
     text: 'typescript',
-    url: './languages/typescript.png',
+    url: './images/languages/typescript.png',
   },
 ];
 
@@ -68,16 +68,42 @@ const $languageSelect = document.querySelector(
 ) as HTMLSelectElement;
 
 function createOptions(data: any, target: any) {
-  const created = data.map((option: any) => `<option>${option.text}</option>`);
+  const created = data.map(
+    (option: any) => `<option value=${option.text}>${option.text}</option>`
+  );
 
   target.innerHTML = created.join('');
 }
 
-function handleChange() {}
+function renderImage(target: HTMLElement, src: string) {
+  if (target.lastElementChild?.tagName === 'IMG')
+    target.lastElementChild.remove();
+
+  const $image = document.createElement('img');
+  $image.src = src;
+  $image.classList.add('image');
+  target.append($image);
+}
+
+function handleAnimalChange(e: any) {
+  const selected: string = e.target.value;
+  const src = animalOptions.filter((option) => option.text === selected)[0].url;
+  const $section = document.querySelector('#animal-section') as HTMLElement;
+  renderImage($section, src);
+}
+
+function handleLanguageChange(e: any) {
+  const selected: string = e.target.value;
+  const src = languageOptions.filter((option) => option.text === selected)[0]
+    .url;
+  console.log(src);
+  const $section = document.querySelector('#language-section') as HTMLElement;
+  renderImage($section, src);
+}
 
 function init() {
-  $animalSelect.addEventListener('onchange', handleChange);
-  $languageSelect.addEventListener('onchange', handleChange);
+  $animalSelect.addEventListener('change', handleAnimalChange);
+  $languageSelect.addEventListener('change', handleLanguageChange);
 
   createOptions(animalOptions, $animalSelect);
   createOptions(languageOptions, $languageSelect);
